7fffcba1ccceb5d7d112312641b2f14104bdafb6
wendy0107 <you@example.com>
2022-07-27 19:35:54 +0800

    integrated preset-timer

---
+++ src/components/AddPresetModal.js
@@ -0,0 +1,143 @@
+import {
+    TouchableWithoutFeedback,
+    KeyboardAvoidingView,
+    Keyboard,
+    TextInput,
+    View,
+    Text,
+    StyleSheet,
+    Pressable,
+} from "react-native";
+import OutIconTextInput from "./OutIconTextInput";
+import ActionButton from "./ActionButton";
+import { BottomSheet } from "react-native-btr";
+import React from "react";
+import { globalStyles } from "../styles/global";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { Ionicons } from "@expo/vector-icons";
+import { TimePicker } from 'react-native-simple-time-picker';
+
+export default function AddPresetModal(props) {
+    const [toDoTitle, setToDoTitle] = React.useState("");
+    const [toDoTask, setToDoTask] = React.useState("");
+
+    const [minutes, setMinutes] = React.useState(0);
+    const [seconds, setSeconds] = React.useState(0);
+
+    const [toDoTask2, setToDoTask2] = React.useState(null);
+    const [minutes2, setMinutes2] = React.useState(0);
+    const [seconds2, setSeconds2] = React.useState(0);
+
+    const [toDoTask3, setToDoTask3] = React.useState(null);
+    const [minutes3, setMinutes3] = React.useState(0);
+    const [seconds3, setSeconds3] = React.useState(0);
+
+    const handleChange = (value: { minutes: number, seconds: number }) => {
+        setMinutes(value.minutes);
+        setSeconds(value.seconds);
+    };
+
+    const handleChange2 = (value: { minutes: number, seconds: number }) => {
+        setMinutes2(value.minutes);
+        setSeconds2(value.seconds);
+    };
+
+    const handleChange3 = (value: { minutes: number, seconds: number }) => {
+        setMinutes3(value.minutes);
+        setSeconds3(value.seconds);
+    };
+
+    const handleReset = () => {
+        setMinutes(0);
+        setSeconds(0);
+        setMinutes2(0);
+        setSeconds2(0);
+        setMinutes3(0);
+        setSeconds3(0);
+    };
+
+    return (
+        <BottomSheet
+            visible={props.visible}
+            onBackButtonPress={props.toggleBottomNavigationView}
+            onBackdropPress={props.toggleBottomNavigationView}
+        >
+            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+                <KeyboardAvoidingView
+                    style={globalStyles.addPresetModal}
+                    enabled={false}
+                >
+                    <TextInput
+                        placeholder="Task Title"
+                        style={globalStyles.textInputContainer}
+                        onChangeText={setToDoTitle}
+                    />
+                    <OutIconTextInput
+                        iconName={"document-text-outline"}
+                        activity={"Add a description or comment"}
+                        onChangeText={setToDoTask}
+                    />
+                    <View style={globalStyles.timePickerStyle}>
+                        <TimePicker
+                            value={{ minutes: minutes, seconds: seconds }}
+                            onChange={handleChange}
+                            pickerShows={["minutes", "seconds"]}
+                            zeroPadding
+                        />
+                    </View>
+                    <OutIconTextInput
+                        iconName={"document-text-outline"}
+                        activity={"Add a description or comment"}
+                        onChangeText={setToDoTask2}
+                    />
+                    <View style={globalStyles.timePickerStyle}>
+                        <TimePicker
+                            value={{ minutes: minutes2, seconds: seconds2 }}
+                            onChange={handleChange2}
+                            pickerShows={["minutes", "seconds"]}
+                            zeroPadding
+                        />
+                    </View>
+                    <OutIconTextInput
+                        iconName={"document-text-outline"}
+                        activity={"Add a description or comment"}
+                        onChangeText={setToDoTask3}
+                    />
+                    <View style={globalStyles.timePickerStyle}>
+                        <TimePicker
+                            value={{ minutes: minutes3, seconds: seconds3 }}
+                            onChange={handleChange3}
+                            pickerShows={["minutes", "seconds"]}
+                            zeroPadding
+                        />
+                    </View>
+
+                    <ActionButton
+                        style={globalStyles.nextButton}
+                        text="Continue"
+                        onPress={() => {
+                            props.onPress(
+                                toDoTitle,
+                                toDoTask,
+                                minutes,
+                                seconds,
+                                toDoTask2,
+                                minutes2,
+                                seconds2,
+                                toDoTask3,
+                                minutes3,
+                                seconds3
+                            );
+                            setToDoTitle(null);
+                            setToDoTask(null);
+                            setToDoTask2(null);
+                            setToDoTask3(null);
+                            handleReset();
+                            props.toggleBottomNavigationView();
+                        }}
+                    />
+                </KeyboardAvoidingView>
+            </TouchableWithoutFeedback>
+        </BottomSheet>
+    );
+}
--- src/screens/Timer.js
+++ src/screens/Timer.js
@@ -10,18 +10,22 @@ import {
 import { globalStyles } from "../styles/global";
 import AddTaskButton from "../components/AddTaskButton";
 import AddTimerModal from "../components/AddTimerModal";
+import AddPresetModal from "../components/AddPresetModal";
 import DumplingSelection from "../components/ChooseDumplingModal";
 import CountDown from "react-native-countdown-component";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { doc, getDoc, updateDoc, setDoc } from "firebase/firestore";
 import { auth, db } from "../firebase/index";
+import useState from "react-usestateref";
 
 export default function Timer() {
   const [visibleDumplingSelection, setVisibleDumplingSelection] =
     React.useState(false);
   const [visibleAddTimer, setVisibleAddTimer] = React.useState(false);
+  const [visibleAddPreset, setVisibleAddPreset] = React.useState(false);
   const [singleTimerList, setSingleTimerList] = React.useState([]);
-  const [timerRunning, setTimer] = React.useState(false);
+  const [multiTimerList, setMultiTimerList] = React.useState([]);
+  const [timerRunning, setTimer, timerRunningRef] = useState(false);
   const [timerTime, setTimerTime] = React.useState(0);
   const [countDownId, setCountDownId] = React.useState(undefined);
   const [elapsedTime, setElapsedTime] = React.useState(0);
@@ -32,6 +36,30 @@ export default function Timer() {
   const [longImage, setLongImage] = React.useState(
     require("../../assets/long-default-pau.png")
   );
+  const [isMultiTimer, setIsMultiTimer, isMultiTimerRef] = useState(false);
+  const [multiTimerState, setMultiTimerState, multiTimerStateRef] = useState(0);
+  const [multiItem, setMultiItem, multiItemRef] = useState(null);
+
+  const reminders = [
+    "Remember to hydrate!",
+    "Remember to rest your eyes!",
+    "Grab a quick snack!",
+    "Time to focus!",
+    "Put your phone down and focus!",
+  ];
+  const [reminder, setReminder, reminderRef] = useState("Time to focus!");
+  const intervalCycle_MS = 300000;
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      let newReminder = reminderRef.current;
+      while (newReminder == reminderRef.current) {
+        newReminder = reminders[Math.floor(Math.random() * reminders.length)];
+      }
+      setReminder(newReminder);
+    }, intervalCycle_MS);
+
+    return () => clearInterval(interval);
+  }, []);
 
   React.useEffect(() => {
     const id = new Date().getTime().toString();
@@ -46,27 +74,104 @@ export default function Timer() {
     setVisibleAddTimer(!visibleAddTimer);
   };
 
+  const toggleAddPresetView = () => {
+    setVisibleAddPreset(!visibleAddPreset);
+  };
+
   {
     /* To toggle on start and reset event for timer */
   }
   const toggleTimer = () => {
-    if (timerRunning) setTimerTime(0);
-    setTimer(!timerRunning);
+    if (timerRunningRef.current) setTimerTime(0);
+    setTimer(!timerRunningRef.current);
   };
 
-  const pausePlayTimer = () => {
-    setTimer(!timerRunning);
+  // const pausePlayTimer = () => {
+  //   setTimer(!timerRunningRef.current);
+  // };
+
+  {
+    /* Remove item from Single Timer List */
+  }
+  let removeItem = (title: String) => {
+    const filteredData = singleTimerList.filter((item) => item.title !== title);
+    setSingleTimerList([...filteredData]);
+  };
+
+  {
+    /* Remove item from Multi Timer List */
+  }
+  let removeMultiItem = (title: String) => {
+    const filteredData = multiTimerList.filter((item) => item.title !== title);
+    setMultiTimerList([...filteredData]);
   };
 
   {
     /* Set Time from Timer */
   }
   let setTime = (minutes, seconds) => {
+    setIsMultiTimer(false);
+    setMultiItem(null);
+    setMultiTimerState(0);
+
     if (timerTime == minutes * 60 + seconds) {
       const id = new Date().getTime().toString();
       setCountDownId(id);
     }
     setTimerTime(minutes * 60 + seconds);
+  };
+
+  {
+    /* Set MultiTime */
+  }
+  let setMultiTime = (item, running=false) => {
+    // Check if initial task
+    if ((multiTimerStateRef.current == 0 && multiItemRef.current == null) || multiItemRef.current != item || !running) {
+      setIsMultiTimer(true);
+      setMultiItem(item);
+      setMultiTimerState(1);
+    }
+
+    // Check multiTimer task count versus current state
+    if (multiTimerStateRef.current == 1) {
+      if (timerTime == item.minutes * 60 + item.seconds) {
+        const id = new Date().getTime().toString();
+        setCountDownId(id);
+      }
+      setTimerTime(item.minutes * 60 + item.seconds);
+      if (running) {
+        toggleTimer();
+      }
+    } else if (multiTimerStateRef.current == 2 && item.task2 != null) {
+      if (timerTime == item.minutes2 * 60 + item.seconds2) {
+        const id = new Date().getTime().toString();
+        setCountDownId(id);
+      }
+      setTimerTime(item.minutes2 * 60 + item.seconds2);
+      if (running) {
+        toggleTimer();
+      }
+    } else if (multiTimerStateRef.current == 3 && item.task3 != null) {
+      if (timerTime == item.minutes3 * 60 + item.seconds3) {
+        const id = new Date().getTime().toString();
+        setCountDownId(id);
+      }
+      setTimerTime(item.minutes3 * 60 + item.seconds3);
+      if (running) {
+        toggleTimer();
+      }
+    } else if (multiTimerStateRef.current == 1) {
+    } else {
+      setIsMultiTimer(false);
+      setMultiItem(null);
+    }
+  };
+
+  {
+    /* Choose between Single and Multi Timer Completion */
+  }
+  let checkFinish = () => {
+    isMultiTimerRef.current ? multiTimerFinish() : timerFinish();
   };
 
   {
@@ -79,24 +184,96 @@ export default function Timer() {
   };
 
   {
+    /* MultiTimer Part Completion */
+  }
+  let multiTimerFinish = () => {
+    alert("Segment Over!");
+    toggleTimer();
+    // Set next stage
+    if (multiTimerStateRef.current < 3) {
+        setMultiTimerState(multiTimerStateRef.current + 1);
+    } else {
+        setMultiTimerState(0);
+    }
+    // Check if has next segment
+    setMultiTime(multiItemRef.current, running=true);
+  };
+
+  {
+    /* Choose between Timer List to add */
+  }
+  let addToTimerList = async (
+    title: String,
+    task: String,
+    minutes: int,
+    seconds: int,
+    task2: String,
+    minutes2: int,
+    seconds2: int,
+    task3: String,
+    minutes3: int,
+    seconds3: int
+  ) => {
+    if (task2 == null) {
+      addToSingleTimerList(title, task, minutes, seconds);
+    } else {
+      addToMultiTimerList(
+        title,
+        task,
+        minutes,
+        seconds,
+        task2,
+        minutes2,
+        seconds2,
+        task3,
+        minutes3,
+        seconds3
+      );
+    }
+  };
+
+  {
     /* Add Single Timer to Single Timer List */
   }
-  // let addToSingleTimerList = async (title, task, minutes, seconds) => {
-  //   const singleTimer = {
-  //     title: title,
-  //     task: task,
-  //     minutes: minutes,
-  //     seconds: seconds,
-  //   };
-  //   setSingleTimerList([...singleTimerList, singleTimer]);
-  // };
+  let addToSingleTimerList = async (title, task, minutes, seconds) => {
+    const singleTimer = {
+      title: title,
+      task: task,
+      minutes: minutes,
+      seconds: seconds,
+    };
+    setSingleTimerList([...singleTimerList, singleTimer]);
+  };
 
-  /* Remove item from Single Timer List */
-  // }
-  // let removeItem = (title) => {
-  //   const filteredData = singleTimerList.filter((item) => item.title !== title);
-  //   setSingleTimerList([...filteredData]);
-  // };
+  {
+    /* Add Multi Timer to Multi Timer List */
+  }
+  let addToMultiTimerList = async (
+    title: String,
+    task: String,
+    minutes: int,
+    seconds: int,
+    task2: String,
+    minutes2: int,
+    seconds2: int,
+    task3: String,
+    minutes3: int,
+    seconds3: int
+  ) => {
+    const multiTimer = {
+      title: title,
+      task: task,
+      minutes: minutes,
+      seconds: seconds,
+      task2: task2,
+      minutes2: minutes2,
+      seconds2: seconds2,
+      task3: task3,
+      minutes3: minutes3,
+      seconds3: seconds3,
+    };
+    setMultiTimerList([...multiTimerList, multiTimer]);
+  };
 
   function getImage(name) {
     if (name === "Bao") {
@@ -130,13 +307,13 @@ export default function Timer() {
           <Icon
             size={60}
             name="stop-circle-outline"
-            style={{ paddingRight: 30 }}
+            style={{ paddingRight: 0 }}
           />
         </Pressable>
 
-        <Pressable onPress={pausePlayTimer} disabled={timerTime == 0}>
+        {/* <Pressable onPress={pausePlayTimer} disabled={timerTime == 0}>
           <Icon size={60} name="play-pause" style={{ paddingLeft: 30 }} />
-        </Pressable>
+        </Pressable> */}
       </View>
     );
   };
@@ -199,6 +376,15 @@ export default function Timer() {
       longestSession: checkLongestSessions(sessionData.longestSession),
       data: updateData(sessionData.data),
     });
+
+    const numOfCoinsEarned = Math.floor(duration / 600) * 5;
+    const coinRef = doc(db, "coins", auth.currentUser.uid);
+    const bar = await getDoc(coinRef);
+    const coinData = bar.data();
+
+    const coinSnap = await updateDoc(coinRef, {
+      coins: coinData.coins + numOfCoinsEarned,
+    });
   };
 
   React.useEffect(() => {
@@ -225,7 +411,7 @@ export default function Timer() {
     >
       <View style={globalStyles.timerContainer}>
         <Text style={globalStyles.subHeaderText}>
-          {timerRunning ? "" : "Choose the menu!"}
+          {timerRunningRef.current ? "" : "Choose the menu!"}
         </Text>
 
         <TouchableHighlight onPress={toggleDumplingSelectionView}>
@@ -233,7 +419,7 @@ export default function Timer() {
         </TouchableHighlight>
 
         <Text style={globalStyles.subHeaderText}>
-          {timerRunning ? "Time to focus!" : "Set your focus session"}
+          {timerRunningRef.current ? reminder : "Set your focus session"}
         </Text>
 
         {/* Timer component */}
@@ -242,7 +428,7 @@ export default function Timer() {
           size={35}
           until={timerTime}
           onFinish={() => {
-            timerFinish();
+            checkFinish();
             recordFinishedSession(timerTime);
           }}
           digitStyle={{ backgroundColor: "#FFF" }}
@@ -251,7 +437,7 @@ export default function Timer() {
           separatorStyle={{ color: "#000" }}
           timeToShow={["H", "M", "S"]}
           timeLabels={{ m: null, s: null }}
-          running={timerRunning}
+          running={timerRunningRef.current}
           showSeparator
           onPress={() => {
             toggleAddTimerView();
@@ -260,10 +446,61 @@ export default function Timer() {
             setElapsedTime(until);
           }} // keeps track of duration passed
         />
-        <View>{timerRunning ? mediaRunning() : mediaNotRunning()}</View>
+        <View>{timerRunningRef.current ? mediaRunning() : mediaNotRunning()}</View>
       </View>
 
-      {!timerRunning && (
+      {!timerRunningRef.current && (
+        <View>
+          {/* List of Multi Timer Settings */}
+          <View style={globalStyles.multiTimerList}>
+            <FlatList
+              data={multiTimerList}
+              horizontal={true}
+              keyExtractor={(item) => item.title}
+              renderItem={({ item }) => (
+                <View style={globalStyles.multiListItem}>
+                  <Text style={globalStyles.multiTimerText}>{item.title}</Text>
+                  <Pressable
+                    style={globalStyles.multiTimerButton}
+                    onPress={() => {
+                      setMultiTime(item);
+                    }}
+                    onLongPress={() => {
+                      removeMultiItem(item.title);
+                    }}
+                  >
+                    <Text style={globalStyles.multiTimerTime}>
+                      {item.task +
+                        " " +
+                        String(item.minutes).padStart(2, "0") +
+                        ":" +
+                        String(item.seconds).padStart(2, "0")}
+                    </Text>
+                    <Text style={globalStyles.multiTimerTime}>
+                      {item.task2 +
+                        " " +
+                        String(item.minutes2).padStart(2, "0") +
+                        ":" +
+                        String(item.seconds2).padStart(2, "0")}
+                    </Text>
+                    {item.task3 != null && (
+                      <Text style={globalStyles.multiTimerTime}>
+                        {item.task3 +
+                          " " +
+                          String(item.minutes3).padStart(2, "0") +
+                          ":" +
+                          String(item.seconds3).padStart(2, "0")}
+                      </Text>
+                    )}
+                  </Pressable>
+                </View>
+              )}
+            />
+          </View>
+        </View>
+      )}
+
+      {!timerRunningRef.current && (
         <View>
           {/* List of Single Timer Settings */}
           <View style={globalStyles.singleTimerList}>
@@ -298,14 +535,22 @@ export default function Timer() {
 
       {/* AddTaskButton at botton right of
                 the screen to bring up the AddTimerModal. */}
-      {/* {!timerRunning && <AddTaskButton onPress={toggleAddTimerView} />} */}
+      {!timerRunningRef.current && <AddTaskButton onPress={toggleAddPresetView} />}
 
-      {/* Modal brought up by the AddTaskButton. Allows users
-                to add single timers to their single timer list */}
+      {/* Modal brought up by the selecting the timer. Allows users
+                to set an adhoc timer */}
       <AddTimerModal
         visible={visibleAddTimer}
         toggleBottomNavigationView={toggleAddTimerView}
         onPress={setTime}
+      />
+
+      {/* Modal brought up by the AddTaskButton. Allows users
+                to add single timers to their single timer list */}
+      <AddPresetModal
+        visible={visibleAddPreset}
+        toggleBottomNavigationView={toggleAddPresetView}
+        onPress={addToTimerList}
       />
 
       {/* Modal to perform dumpling selection. Allows users
